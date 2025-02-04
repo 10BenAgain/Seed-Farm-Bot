57ee4a35db85c7cdd490fee5646845f503bb44cf
Ben <109313313+10BenAgain@users.noreply.github.com>
2025-02-04 10:27:52 -0700

    Add comments for magic numbers

--- main.c
+++ main.c
@@ -2,12 +2,25 @@
 #include <stdint.h>
 #include <stddef.h>
 
+
+/* How Frame Interval is calculated
+ * 59.65549883230804 FPS = 16.76291405778042 MS = 0.016762914057780419624 Seconds
+ * Target period is 0.016762914057780419624
+ * Rounding down to 0.016762 should be accurate enough,
+ * and it fits nicely with a prescalar of 8 (16 MHz or 16000000 Hz)
+ * New clock speed = 0.016762 * (16000000 / 8 )
+ * 0.016762 * 2000000 = 33524
+ * Now we just need to check every time the TCNT1 register matches the calculated value.
+ * To increment the target seed timer by smaller intervals, we can simply divide the
+ * result interval by even divisors (33524 / 2, 4) where 2 = 16762 and 4 = 8381
+*/
+
 #define QUARTER
 #define INCREMENT
 #define SEEDS_TO_STORE      1000
-#define START               2088
-#define LOAD_INTO_GAME      260
-#define WAIT_FOR_SAVE_MENU  360
+#define START               2088 // Starting point, approximately 35000 MS
+#define LOAD_INTO_GAME      260  // How many frames it takes to get from save select to overworld
+#define WAIT_FOR_SAVE_MENU  360  // How long to hold seed button down for after timer is up to get to save select menu
 
 #define OR_EQ_LS1(x,y)  ((x)|=(1<<(y)));
 
